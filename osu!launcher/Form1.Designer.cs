f9087a5a05603e4f9d8dd28b16be888faa2064f8
Puk06 <86549420+puk06@users.noreply.github.com>
2023-08-30 17:26:21 +0900

    Add files via upload

---
+++ osu!launcher/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace osu_launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.osulaunch = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // osulaunch
+            // 
+            this.osulaunch.BackColor = System.Drawing.SystemColors.Control;
+            this.osulaunch.Font = new System.Drawing.Font("PhrasticMedium", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.osulaunch.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.osulaunch.Image = ((System.Drawing.Image)(resources.GetObject("osulaunch.Image")));
+            this.osulaunch.Location = new System.Drawing.Point(182, 129);
+            this.osulaunch.Name = "osulaunch";
+            this.osulaunch.Size = new System.Drawing.Size(175, 51);
+            this.osulaunch.TabIndex = 0;
+            this.osulaunch.UseVisualStyleBackColor = false;
+            this.osulaunch.Click += new System.EventHandler(this.Osulaunch_pushed);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("HackGen", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(182, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label1.Location = new System.Drawing.Point(37, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "osu! Folder";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(182, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "サーバーの保存";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(182, 106);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(93, 16);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "フォルダの保存";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("HackGen", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(182, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 27);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 27);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "自動検出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(551, 192);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.osulaunch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "osu! Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button osulaunch;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ osu!launcher/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using System.Drawing;
+using System.Linq;
+
+namespace osu_launcher
+{
+    public partial class Form1 : Form
+    {
+        private static Process _osuprocess;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //ウィンドウの設定
+            ResumeLayout(false);
+            PerformLayout();
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+
+            //label1の設定
+            label1.ForeColor = Color.FromArgb(255, 255, 255);
+            label1.BackColor = Color.Transparent;
+
+            //label3の設定 
+            label3.ForeColor = Color.FromArgb(255, 255, 255);
+            label3.BackColor = Color.Transparent;
+
+            //checkBox1,2の設定
+            checkBox1.ForeColor = Color.FromArgb(255, 255, 255);
+            checkBox1.BackColor = Color.Transparent;
+            checkBox1.Enabled = false;
+            checkBox2.ForeColor = Color.FromArgb(255, 255, 255);
+            checkBox2.BackColor = Color.Transparent;
+            if (textBox1.Text == "")
+            {
+                checkBox2.Enabled = false;
+            }
+
+            //サーバー一覧を取得し、comboBox1に追加
+            StreamReader streamReader = new StreamReader("./serverdata.json", Encoding.GetEncoding("Shift_JIS"));
+            string str = streamReader.ReadToEnd();
+            streamReader.Close();
+            JObject serverdata = JObject.Parse(str);
+
+            //osu!のインストール先を取得
+            StreamReader osustreamReader = new StreamReader("./osulocation.txt", Encoding.GetEncoding("Shift_JIS"));
+            string locationstr = osustreamReader.ReadToEnd();
+            osustreamReader.Close();
+
+            //osu!のインストール先をtextBox1に、サーバーをcomboBox1に追加
+            comboBox1.Text = "osu!bancho";
+            textBox1.Text = locationstr;
+
+            //サーバー一覧をcomboBox1に追加
+            foreach (var server in serverdata["links"])
+            {
+                comboBox1.Items.Add(server);
+            }
+        }
+
+        private void Osulaunch_pushed(object sender, EventArgs e)
+        {
+            try
+            {
+                //osu!のインストール先を取得
+                StreamReader osustreamReader = new StreamReader("./osulocation.txt", Encoding.GetEncoding("Shift_JIS"));
+                string locationstr = osustreamReader.ReadToEnd();
+                osustreamReader.Close();
+
+                //osu!のインストール先とサーバーの指定(サーバーはプラベ鯖メインで考えてある)
+                string osulocation = locationstr.Replace("/osu!.exe", "");
+                string serverlocation = $"-devserver {comboBox1.Text}";
+
+                //osu!のインストール先の指定がない場合の処理
+                if (locationstr == "" && textBox1.Text == "") //osu!のインストール先が保存されてない、かつ指定されていない場合
+                {
+                    MessageBox.Show(@"osu!のインストール先を指定してください。", "エラー");
+                    return;
+                }
+                else if (locationstr != "" && textBox1.Text == "") //osu!のインストール先が保存されている、かつ指定されていない場合
+                {
+                    osulocation = locationstr;
+                }
+                else if (locationstr != "" && textBox1.Text != "") //osu!のインストール先が保存されている、かつ指定されている場合。この場合は入力された方を優先して指定する
+                {
+                    osulocation = textBox1.Text.Replace("/osu!.exe", "");
+                }
+                else //とりあえず指定された方を優先して指定する
+                {
+                    osulocation = textBox1.Text.Replace("/osu!.exe", "");
+                }
+
+                if (comboBox1.Text == @"osu!bancho" | comboBox1.Text == "") //サーバーがosu!bancho、または指定されていない場合はサーバーの指定をせず、そのまま起動する
+                {
+                    serverlocation = "";
+                }
+
+                if (checkBox1.Checked) //サーバーの登録チェックボックスがチェックされている場合
+                {
+                    //サーバーの登録
+                    StreamReader streamReader = new StreamReader("./serverdata.json", Encoding.GetEncoding("Shift_JIS"));
+                    string str = streamReader.ReadToEnd();
+                    streamReader.Close();
+                    JObject serverdata = JObject.Parse(str);
+                    serverdata["links"].Last.AddAfterSelf(comboBox1.Text);
+                    Encoding enc = Encoding.GetEncoding("Shift_JIS");
+                    StreamWriter writer = new StreamWriter("./serverdata.json", false, enc);
+                    string jsonData = JsonConvert.SerializeObject(serverdata, Formatting.Indented);
+                    writer.Write(jsonData);
+                    writer.Close();
+                    comboBox1.Items.Add(comboBox1.Text);
+                    MessageBox.Show("サーバーの登録が完了しました。", "サーバー登録完了");
+                    checkBox1.Checked = false;
+                }
+
+                if (checkBox2.Checked) //osu!のインストール先の保存チェックボックスがチェックされている場合
+                {
+                    StreamWriter osustreamWriter = new StreamWriter("./osulocation.txt", false, Encoding.GetEncoding("Shift_JIS"));
+                    osustreamWriter.Write(textBox1.Text.Replace("/osu!.exe", ""));
+                    osustreamWriter.Close();
+                }
+
+                if (Process.GetProcessesByName("osu!").Length > 0) //osu!が既に起動している場合
+                {
+                    MessageBox.Show("osu!は既に起動中です。一度osu!を閉じてから起動してください。", "起動できません！");
+                    return;
+                }
+
+                StreamReader serverReader = new StreamReader("./serverdata.json", Encoding.GetEncoding("Shift_JIS"));
+                string serverString = serverReader.ReadToEnd();
+                serverReader.Close();
+                JObject serverRawData = JObject.Parse(serverString);
+                string[] serverlistarray = serverRawData["links"].ToObject<string[]>();
+
+                checkBox2.Enabled = textBox1.Text != locationstr;
+                checkBox1.Enabled = !ArrayContains(serverlistarray, comboBox1.Text);
+
+                //osu!の起動
+                _osuprocess = new Process();
+                _osuprocess.StartInfo.FileName = $"{osulocation}/osu!.exe";
+                _osuprocess.StartInfo.Arguments = serverlocation;
+                _osuprocess.StartInfo.UseShellExecute = false;
+                _osuprocess.Start();
+            }
+            catch (Exception)　//osu!.exeが見つからなかった場合
+            {
+                MessageBox.Show("フォルダからosu!.exeが見つかりませんでした。", "エラー");
+            }
+        }
+
+        private bool ArrayContains(string[] array, string value)
+        {
+            foreach (string item in array)
+            {
+                if (item == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            //サーバーの登録
+            StreamReader streamReader = new StreamReader("./serverdata.json", Encoding.GetEncoding("Shift_JIS"));
+            string str = streamReader.ReadToEnd();
+            streamReader.Close();
+            JObject serverdata = JObject.Parse(str);
+            string[] serverarray = serverdata["links"].ToObject<string[]>();
+            if (comboBox1.Text == "")
+            {
+                checkBox1.Enabled = false;
+                return;
+            }
+            checkBox1.Enabled = !ArrayContains(serverarray, comboBox1.Text); //既に登録されている場合
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //osu!のインストール先を取得
+            StreamReader osustreamReader = new StreamReader("./osulocation.txt", Encoding.GetEncoding("Shift_JIS"));
+            string locationstr = osustreamReader.ReadToEnd();
+            osustreamReader.Close();
+            if (textBox1.Text == "")
+            {
+                checkBox2.Enabled = false;
+                return;
+            }
+            checkBox2.Enabled = textBox1.Text != locationstr;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string processName = "osu!";
+
+            string processPath = GetProcessPath(processName);
+
+            if (!string.IsNullOrEmpty(processPath))
+            {
+                textBox1.Text = processPath.Replace(@"\osu!.exe", "");
+            }
+            else
+            {
+                MessageBox.Show("osu!.exeが見つかりませんでした。起動してからもう一度お試しください。", "エラー");
+            }
+        }
+
+        static string GetProcessPath(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            if (processes.Length > 0)
+            {
+                string processPath = processes[0].MainModule.FileName;
+                return processPath;
+            }
+
+            return null;
+        }
+    }
+}
